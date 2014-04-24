46c82ac510dfd629b2a125903695f321b7d7f17a
hmscj <hmscj@cin.ufpe.br>
2014-04-24 09:37:51 -0300

    pushing jumps examples on fool class.

--- src/instrumentation/examples/Foo.java
+++ src/instrumentation/examples/Foo.java
@@ -1,10 +1,34 @@
 package instrumentation.examples;
 
 public class Foo {
-  static int foo(int a, int b) {
-    return a - b;
+  static void foo(int a, int b, int c) {
+    //IP L1 Q1 ->Maior,medio,menor
+    int maior, medio, menor;
+    medio = 0;
+    if (a <= b){
+      maior = b;
+      menor = a;
+    } else {
+      maior = a;
+      menor = b;
+    } if ( c >= maior){
+      medio = maior;
+      maior = c;
+    } else if (c < menor){
+      medio = menor;
+      menor = c;
+    }
+     if (maior == menor){
+       maior = maior + 1;
+     } else {
+      menor = medio - 1;
+     }
+     if (medio > menor){
+       medio = medio + 1;
+     }
+     
   }
   public static void main(String[] args) {
-    int a = foo(5, 10);
+    foo(5, 10, 7);
   }
 }
