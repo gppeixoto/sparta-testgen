72cbf8235cbe1da7c88c7943a210e0a398122a71
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2014-05-23 16:15:43 -0300

    refactorings

--- src/replayer/Main.java
+++ src/replayer/Main.java
@@ -21,10 +21,10 @@ public class Main {
   OperandStack operandStack;
   CallStack callStack;
   StaticArea sa;
-  
+
   public Main(List<String> input) {
     instructionTrace = input;
-    
+
     heap = new Heap(); 
     callStack = new CallStack();
     operandStack = callStack.push("main");
@@ -33,7 +33,7 @@ public class Main {
   }
 
   public static void main(String[] args) throws Exception {
-    
+
     // read trace
     List<String> buffer = new ArrayList<String>();
     BufferedReader br = new BufferedReader(new FileReader("trace.out"));
@@ -42,14 +42,14 @@ public class Main {
       buffer.add(s.trim());
     }
     br.close();   
-    
+
     // replay
     try {
       (new Main(buffer)).replay();
     } catch (FinishedExecutionException _) {
       System.out.println("  execution replayed");
     }
-    
+
   }
 
   enum OPCODE {BIPUSH, ISTORE, ILOAD, ICONST, 
@@ -59,136 +59,128 @@ public class Main {
     INEG, IAND, IOR, ISHL, ISHR, IUSHR, IXOR, LCMP, IF, GOTO, 
     FRAME, ANEWARRAY, AASTORE, PUTSTATIC, GETFIELD, AALOAD, SIPUSH,
     DSTORE, DLOAD, DMUL, DADD, DDIV, DSUB, LOOKUPSWITCH, INVOKEVIRTUAL
-    };
-    
-    /**
-     * ACONST_NULL
-     * ARETURN
-     * ARRAYLENGTH
-     * ATHROW
-     * BALOAD
-     * BASTORE
-     * BREAKPOINT
-     * CALOAD
-     * CASTORE
-     * CHECKCAST
-     * D2F
-     * D2I
-     * D2L
-     * DALOAD
-     * DASTORE
-     * DCMPG
-     * DCMPL
-     * DCONST
-     * DNEG
-     * DREM
-     * DRETURN
-     * DUP_X1
-     * DUP_X2
-     * DUP2
-     * DUP2_X1
-     * DUP2_X2
-     * F2D
-     * F2I
-     * F2L
-     * FADD
-     * FALOAD
-     * FASTORE
-     * FCMPG
-     * FCMPL
-     * FCONST
-     * FDIV
-     * FLOAD
-     * FMUL
-     * FNEG
-     * FREM
-     * FRETURN
-     * FSTORE
-     * FSUB
-     * ... // CONTINUE AFTER F
-     */
+  };
 
-  public void replay() {
-    
-    Map<String, Integer> labels = new HashMap<String, Integer>();
-    
-    for(int i = 0; i < instructionTrace.size(); i++) {
-      String insn = instructionTrace.get(i);
-      
-      String[] nameIns = insn.split(":"); 
-//      System.out.printf("%s : %s\n", nameIns[0], nameIns[1]);
-      insn = nameIns[1].trim();
+  /**
+   * ACONST_NULL
+   * ARETURN
+   * ARRAYLENGTH
+   * ATHROW
+   * BALOAD
+   * BASTORE
+   * BREAKPOINT
+   * CALOAD
+   * CASTORE
+   * CHECKCAST
+   * D2F
+   * D2I
+   * D2L
+   * DALOAD
+   * DASTORE
+   * DCMPG
+   * DCMPL
+   * DCONST
+   * DNEG
+   * DREM
+   * DRETURN
+   * DUP_X1
+   * DUP_X2
+   * DUP2
+   * DUP2_X1
+   * DUP2_X2
+   * F2D
+   * F2I
+   * F2L
+   * FADD
+   * FALOAD
+   * FASTORE
+   * FCMPG
+   * FCMPL
+   * FCONST
+   * FDIV
+   * FLOAD
+   * FMUL
+   * FNEG
+   * FREM
+   * FRETURN
+   * FSTORE
+   * FSUB
+   * ... // CONTINUE AFTER F
+   */
 
-      // parsing instruction string
-      // TODO: optimize this if inefficient
-      String[] splits = insn.split("\\s++|_");
-      OPCODE kind = null;
-      
-      //TODO: optimize this
-      for (OPCODE opcode : OPCODE.values()) {
-        if (opcode.toString().equals(splits[0])) {
-          kind = opcode;
-          break;
-        }
+  public OPCODE lookupOpcode(String str) {   
+    OPCODE kind = null;
+    for (OPCODE opcode : OPCODE.values()) {
+      if (opcode.toString().equals(str)) {
+        kind = opcode;
+        break;
       }
+    }
+    return kind;
+  }
+  
+  public void replay() {
+
+    /**
+     * Mapping from position labels (which can be referenced
+     * from control-flow instructions) to positions in 
+     * the list of instructions (instructionTrace)  
+     */
+    Map<String, Integer> labels = new HashMap<String, Integer>();
+
+    /**
+     * look across all instructions in the trace
+     */
+    for(int i = 0; i < instructionTrace.size(); i++) {
+      
+      String insn = instructionTrace.get(i);
+      String[] nameIns = insn.split(":"); 
+      insn = nameIns[1].trim();
+      String[] splits = insn.split("\\s++|_");
+
+      /**
+       * lookup kind of instruction, raise 
+       * error message if kind not 
+       */
+      OPCODE kind = lookupOpcode(splits[0]);
       if (kind == null) {
         if (splits[0].trim().matches("L\\d*")) {
           labels.put(splits[0], i);
-          continue; // skip
+          continue; // skip instruction
         }
         throw new RuntimeException("Could not find instruction: >" + splits[0] + "<");
       }
 
-      // found kind.  now parse complement if any
-      String complementOne = null;
-      String complementTwo = null;
-      String complementThree = null;
-      if (splits.length > 1) {
-        complementOne = splits[1].trim();
-        if (!complementOne.equals("")) {
-          if (complementOne.startsWith("_")) {
-            complementOne = complementOne.substring(1);
-          }
-        }
-        if (splits.length > 2) {
-          complementTwo = splits[2].trim();
-          if (!complementTwo.equals("")) {
-            if (complementTwo.startsWith("_")) {
-              complementTwo = complementTwo.substring(1);
-            }
-          }
-          if (splits.length > 3) {
-            complementThree = splits[3].trim();
-            if (!complementThree.equals("")) {
-              if (complementThree.startsWith("_")) {
-                complementThree = complementThree.substring(1);
-              }
-            }
-          }
-        }
-      }
-      // done with parsing
-      
+      /**
+       * update complements of instructions
+       */
+      String[] complements = extractComplements(splits);
+      String complementOne = complements[0];
+      String complementTwo = complements[1];
+
       boolean isStatic = false;
-      // replay instruction
+
+      /**
+       * decide which instruction to apply
+       */
       switch (kind) {
-      
+
       case BIPUSH:
         operandStack.push(Integer.parseInt(complementOne));
         break;
-      
+
       case DSTORE:
       case ASTORE:
       case ISTORE:
         operandStack.store(Integer.parseInt(complementOne));
         break;
-      
+
       case DLOAD:
       case ALOAD:
       case ILOAD:
         operandStack.load(Integer.parseInt(complementOne));
         break;
-      
+
       case ICONST:
         boolean neg = false;
         if (complementOne.charAt(0)=='M') {
@@ -198,11 +190,11 @@ public class Main {
         int tmp = Integer.parseInt(complementOne);
         operandStack.push(neg?-tmp:tmp);
         break;
-      
+
       case RETURN:
         operandStack = callStack.pop();
         break;
-      
+
       case ANEWARRAY:
       case NEWARRAY:
         //TODO: ignoring count.  this will be important to reproduce out-of-bounds exceptions
@@ -210,11 +202,11 @@ public class Main {
         // ignoring type for now
         operandStack.push(heap.newCell());
         break;
-      
+
       case DUP:
         operandStack.push(operandStack.peek());
         break;
-      
+
       case AASTORE:
       case IASTORE:
         Object val = operandStack.pop();
@@ -222,14 +214,14 @@ public class Main {
         HeapCell arRef = (HeapCell) operandStack.pop();
         arRef.store(index+"", val);
         break;
-        
+
       case AALOAD:
       case IALOAD:
         index = (Integer) operandStack.pop();
         arRef = (HeapCell) operandStack.pop();
         operandStack.push(arRef.load(index+""));
         break;
-      
+
       case LDC:
 
         try {
@@ -244,43 +236,37 @@ public class Main {
           } 
         }
         break;
-      
+
       case GETSTATIC:
         try {
           int idx = complementOne.lastIndexOf(".");
           String clazz = complementOne.substring(0, idx).replace('/', '.');
           String fieldName = complementOne.substring(idx+1);
-//          Field f = Class.forName(clazz).getDeclaredField(fieldName);
-//          f.setAccessible(true);
-//          operandStack.push(f.get(null));
           operandStack.push(sa.getStatic(Class.forName(clazz), fieldName));
         } catch (Exception e) {
           throw new RuntimeException("check this!");
         }
         break;
-      
+
       case PUTSTATIC:
         try {
           int idx = complementOne.lastIndexOf(".");
           String clazz = complementOne.substring(0, idx).replace('/', '.');
           String fieldName = complementOne.substring(idx+1);
-//          Field f = Class.forName(clazz).getDeclaredField(fieldName);
-//          f.setAccessible(true);
-//          f.set(null, operandStack.pop());
           sa.putStatic(Class.forName(clazz), fieldName, operandStack.pop());
         } catch (Exception e) {
           e.printStackTrace();
           throw new RuntimeException("check this!");
         }
         break;
-      
+
       case NEW: 
         operandStack.push(heap.newCell());
         break;
-      
+
       case INVOKESTATIC:
         isStatic = true;
-      
+
       case INVOKEVIRTUAL:
       case INVOKESPECIAL: 
         int idx = complementOne.lastIndexOf('.');
@@ -288,7 +274,7 @@ public class Main {
         String mName = complementOne.substring(idx+1);
         String[] args = new String[]{cName, mName, complementTwo};
         AccessibleObject aobj = Util.lookup(args);
-        
+
         int numParams; 
         boolean skip = false;
         if (aobj instanceof Method) {
@@ -304,7 +290,7 @@ public class Main {
             skip = true;
           }
         }
-        
+
         List<Object> list = new ArrayList<Object>();
         for (int k = 0; k < numParams; k++) {
           list.add(operandStack.pop());
@@ -322,32 +308,32 @@ public class Main {
           }
         }
         break;
-        
+
       case PUTFIELD: 
         val = operandStack.pop();
         HeapCell objRef = (HeapCell) operandStack.pop();
         String fieldName = complementOne.substring(complementOne.lastIndexOf(".")+1);
         objRef.store(fieldName, val);
         break;
-        
+
       case GETFIELD: 
         objRef = (HeapCell) operandStack.pop();
         fieldName = complementOne.substring(complementOne.lastIndexOf(".")+1);
         operandStack.push(objRef.load(fieldName));
         break;
-        
+
       case LINENUMBER:
         labels.put(complementTwo, i);
         break;
-        
+
       case DADD:
       case DSUB: 
       case DMUL:
       case DDIV:
-        
+
         double d1 = (Double) operandStack.pop();
         double d2 = (Double) operandStack.pop();
-        
+
         double res;
         switch(kind) {
         case DADD:
@@ -365,11 +351,11 @@ public class Main {
         default:
           throw new RuntimeException("Interpretation of Instruction undefined: " + kind);
         }
-        
+
         operandStack.push(res);
         break;
 
-        
+
       case IADD:
       case ISUB: 
       case IMUL:
@@ -381,10 +367,10 @@ public class Main {
       case IAND:
       case IOR:
       case IXOR:
-        
+
         int val1 = (Integer) operandStack.pop();
         int val2 = (Integer) operandStack.pop();
-        
+
         switch(kind) {
         case IADD:
           tmp = val1 + val2;
@@ -422,31 +408,31 @@ public class Main {
         default:
           throw new RuntimeException("Interpretation of Instruction undefined: " + kind);
         }
-        
+
         operandStack.push(tmp);
         break;
-        
+
       case LCMP:
         val1 = (Integer) operandStack.pop();
         val2 = (Integer) operandStack.pop();
         operandStack.push( val1 == val2 ? 0 : (val1 < val2 ? -1 : 1));
-        
+
       case IRETURN:
         val1 = (Integer) operandStack.pop();
         operandStack = callStack.pop();
         operandStack.push(val1);
         break;
-        
+
       case INEG:
         val1 = (Integer) operandStack.pop();
         operandStack = callStack.pop();
         operandStack.push(-val1);
         break;
-        
+
       case POP:
         operandStack.pop();
         break;
-        
+
       case IF:
         val1 = (Integer) operandStack.pop();
         val2 = (Integer) operandStack.pop();
@@ -466,27 +452,27 @@ public class Main {
         } else {
           throw new UnsupportedOperationException("IF >" + op + "<");
         }
-        
+
         if (shouldJump) { // reset program counter
           i = lookupForLabel(labels, complementTwo, i);
         }
         break;
-        
+
       case GOTO:
         i = lookupForLabel(labels, complementOne, i);
-       break;
-        
+        break;
+
       case FRAME:
         // ignore ASM-generated instruction
         break;
-        
+
       case SIPUSH:
         // representing as integer (ignoring overflows)
         operandStack.push(Integer.parseInt(complementOne));
         break;
-        
+
       case LOOKUPSWITCH:
-        
+
         val1 = (Integer) operandStack.pop();
         String[] parts = insn.substring(OPCODE.LOOKUPSWITCH.name().length()+1).trim().split("  ");
         String gotoLabel = null;
@@ -505,7 +491,7 @@ public class Main {
         }
         i = lookupForLabel(labels, gotoLabel, i);
         break;
-       
+
       default:
         throw new RuntimeException("Interpretation of Instruction undefined: " + kind);
       }
@@ -541,6 +527,36 @@ public class Main {
       throw new RuntimeException("ERROR: Could not find requested label!");
     }
     return res;
+  }
+  
+  public String[] extractComplements(String[] splits) {
+    String complementOne, complementTwo, complementThree;
+    complementOne = complementTwo = complementThree = null;
+    if (splits.length > 1) {
+      complementOne = splits[1].trim();
+      if (!complementOne.equals("")) {
+        if (complementOne.startsWith("_")) {
+          complementOne = complementOne.substring(1);
+        }
+      }
+      if (splits.length > 2) {
+        complementTwo = splits[2].trim();
+        if (!complementTwo.equals("")) {
+          if (complementTwo.startsWith("_")) {
+            complementTwo = complementTwo.substring(1);
+          }
+        }
+        if (splits.length > 3) {
+          complementThree = splits[3].trim();
+          if (!complementThree.equals("")) {
+            if (complementThree.startsWith("_")) {
+              complementThree = complementThree.substring(1);
+            }
+          }
+        }
+      }
+    }
+    return new String[]{complementOne, complementTwo, complementThree};
   }
 
 
